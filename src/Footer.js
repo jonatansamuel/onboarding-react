f4bef5505f7e3f29afc9331fc156c2769c93ecb5
jonatansamuel <jonasacru@gmail.com>
2019-09-24 15:19:43 -0700

    first version of signup

---
+++ src/Footer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Footer extends Component{
+  render() {
+    return (
+      <footer>
+        © 2019 MK Decision
+      </footer>
+    )
+  }
+}
+
+export default Footer
---
+++ src/Personal.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+class Personal extends Component{
+  render(){
+    return(
+      <ul>
+        <h2>Personal Information</h2>
+        <li>firstname</li>
+        <li>lastname</li>
+        <li>email</li>
+        <li>phone</li>
+        <li>password</li>
+      </ul>
+    )
+  }
+}
+
+export default Personal
